a0b89711e4bf1ca8b92cada1c053107b43381b79
andjey44 <andjeykh@yandex.ru>
2022-05-18 15:20:51 +0300

    Update
    Correct server client game words

--- tasks021-023/Task022/Task022-server.py
+++ tasks021-023/Task022/Task022-server.py
@@ -1,21 +1,15 @@
 import socket
-
 sock = socket.socket()
-sock.bind(('192.168.43.132', 4000))
+sock.bind(('192.168.0.104', 4000))
 sock.listen(2)
-
 user = [sock.accept()[0], sock.accept()[0]]
 lives = [3, 3]
-
 start, i, words = True, 0, []
 while start:
     user[i].send('Your turn'.encode('UTF-8') if not words else
                  f'Your move to the letter "{words[-1]}"'.encode('UTF-8'))
-
     user[not i].send('We are waiting for the opponent response '.encode('UTF-8'))
-
     word = user[i].recv(1024).decode('UTF-8').lower()
-
     if word in words:
         user[i].send('You can not repeat the words '.encode('UTF-8'))
     elif words and words[-1][-1] != word[0]:
