38008f6b6ed7793869a3d19f50e8b08da2d4f495
andjey44 <andjeykh@yandex.ru>
2022-05-31 21:37:40 +0300

    Update
    Update

--- tasks011-013/Task011.py
+++ tasks011-013/Task011.py
@@ -2,15 +2,15 @@
 class Homework:
     global global_per # global
 
-    def first_metod(self, change_2: float = 3, change_3: float = 5) -> None:
+    def first_metod(self, change_2: float = 3, change_3: float = 5) -> float:
         encl_per: float = 12         # enclosing
 
-        def first_metod_in_menod() -> None:
+        def first_metod_in_menod() -> float:
             local_per: float = 20             # local
-            print(f'общяя сумма = {global_per + encl_per + local_per}')
+            return global_per + encl_per + local_per
 
         first_metod_in_menod()
-        print(f'сумма 2 и 3 = {change_2 + change_3}')
+        return change_2 + change_3
 
 
 global_per: int = 1 # global
