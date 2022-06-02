990d9ff01615dc283ba2d6484c4597e151af5ec6
andjey44 <andjeykh@yandex.ru>
2022-06-02 19:08:05 +0300

    Update
    Last-Update(Correct)

--- tasks003-005/Task003.py
+++ tasks003-005/Task003.py
@@ -1,19 +1,3 @@
-# data = input().split()
-# stack = []
-# for x in data:
-#     if x in '+-*':
-#         z1 = stack.pop()
-#         z2 = stack.pop()
-#         if x == '+':
-#             res = z1 + '+' + z2
-#         elif x == '-':
-#             res = z1 + '-' + z2
-#         else:
-#             res = z1 + '*' + z2
-#         stack.append(res + '')
-#     else:
-#         stack.append(x + '')
-# print(stack[0])
 from pythonds.basic.stack import Stack
 
 
