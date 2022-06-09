b034b438e277a5966f79350cd02fc30b7ea956dd
andjey44 <andjeykh@yandex.ru>
2022-06-09 16:55:39 +0300

    Update
    Last_Update with correct changes

--- tasks014-020/Task016.py
+++ tasks014-020/Task016.py
@@ -1,6 +1,11 @@
 import re
-string3='00:59, 03:15, 04:20, 18:30,25:60, 13:71, 101:02, 01:14'
-s = '1 2 6 23 43 35 67 4 33 21 21 10 45 34'
+from Task019 import z
+# string3='00:59, 03:15, 04:20, 18:30,25:60, 13:71, 101:02, 01:14'
+# s = '1 2 6 23 43 35 67 4 33 21 21 10 45 34'
 def task16(string3):
     return(re.findall(r'\b(?:[0-1][0-9]|2[0-4]):[0-5][0-9]', string3))
-print(task16(string3))
+if __name__ == '__main__':
+    for string3 in z:
+        if task16(string3):
+            print(task16(string3))
+# print(task16(string3))
--- tasks014-020/Task017.py
+++ tasks014-020/Task017.py
@@ -1,5 +1,10 @@
 import re
-string4='ООО, ИП, НИИ МАНАНАЗЭМ, ООН, ИТИС КФУ, ФизРа'
+from Task019 import z
+# string4='ООО, ИП, НИИ МАНАНАЗЭМ, ООН, ИТИС КФУ, ФизРа'
 def task17(string4):
     return(re.findall(r'\b[A-Z А-ЯЁ]{2,}\b', string4))
-print(task17(string4))
+if __name__ == '__main__':
+    for string4 in z:
+        if task17(string4):
+            print(task17(string4))
+# print(task17(string4))
